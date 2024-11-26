58341b2602b289fdbf13523945eb4869a8a0f382
CMingHwee <chuaminghweesg@gmail.com>
2024-11-26 16:27:43 +0800

    Duck Typing.py

---
+++ Duck Typing.py
@@ -0,0 +1,31 @@
+# duck typing = concept where the class of an object is less important than the methods/attributes
+
+class Duck:
+
+    def walk(self):
+        print("This duck is walking")
+
+    def talk(self):
+        print("This duck is qwuacking")
+
+class Chicken:
+
+    def walk(self):
+        print("This chicken is walking")
+
+    def talk(self):
+        print("This chicken is clucking")
+
+class Person():
+
+    def catch(self, duck):
+        duck.walk()
+        duck.talk()
+        print("You caught the critter!")
+
+
+duck = Duck()
+chicken = Chicken()
+person = Person()
+
+person.catch(chicken)
